e95fdbee6fc375e0765a046f477c85f0e49a28bf
devam12 <devampanchasara12@gmail.com>
2025-01-02 09:50:16 +0530

    splash screnn animation done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,25 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Image } from "react-native";
+import React, { useEffect } from "react";
+import { StyleSheet } from "react-native";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function RootLayout() {
+  useEffect(() => {
+    prepareApp();
+  }, []);
+
+  const prepareApp = async () => {
+    try {
+      await SplashScreen.preventAutoHideAsync();
+      await SplashScreen.hideAsync();
+    } catch (error) {
+      console.warn("Error preparing app: ", error);
+    }
+  };
+
   return (
     <Stack initialRouteName="index">
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      {/* <Stack.Screen name="animated" options={{ headerShown: false }} />
-      <Stack.Screen name="circle" options={{ headerShown: false }} /> */}
       <Stack.Screen name="merge" options={{ headerShown: false }} />
     </Stack>
   );
